f48f6e016afcd3321f4abb188c3e1f2bf4397642
sunny <sunnyshah711@gmail.com>
2022-11-27 15:28:33 -0500

    connect to social through app

--- jscore/src/apps/blockbook/Blockbook.ts
+++ jscore/src/apps/blockbook/Blockbook.ts
@@ -1,4 +1,6 @@
 import Core from "../../Core";
+import Social from "../../modules/social/Social";
+import Web3 from "../../modules/web3/Web3";
 import App from "../App";
 
 interface Config {
@@ -12,11 +14,17 @@ interface DependencyInjection {
 
 
 export default class BlockBook extends App {
+  private web3: Web3 | undefined;
+  private social: Social | undefined;
+
   constructor(core : Core<{}>, private config : Config, private dependencyInjection: DependencyInjection) {
     super(core)
   }
   protected async start(){
+    this.social = this.core.modules.social
+    this.web3 = this.core.modules.web3
 
+    this.social?.network?.search()
   }
 
   private publish(){
